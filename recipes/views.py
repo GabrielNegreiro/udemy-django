3cddd659bb0c2eb32204c1ec8d4131d571136b3a
Gabriel Negreiros <serjaoserjao47@gmail.com>
2025-03-20 23:49:02 -0300

    include-head

--- recipes/views.py
+++ recipes/views.py
@@ -4,9 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'recipes/home.html', context={
-        'nome': 'Gabriel'
-    })
-
-def contato(response):
-    return HttpResponse("Contato 1")
+    return render(request, 'recipes/pages/home.html')
